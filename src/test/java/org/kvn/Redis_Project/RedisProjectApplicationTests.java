99e90a676474e7a59a04f7a642b3701f75825878
Raghu <k.raghu9926@gmail.com>
2024-12-11 16:14:53 +0530

    project initial commit

---
+++ src/test/java/org/kvn/Redis_Project/RedisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kvn.Redis_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
